3ded30beabd4e9dde249f4e375d949c155bfc1c8
JoevantiQ <joevanti.quijano49@spsmail.cuny.edu>
2025-03-10 21:02:34 -0400

    Update

---
+++ Assignment6.py
@@ -0,0 +1,24 @@
+import unittest
+from conversions import *
+
+
+class TestConversions(unittest.TestCase):
+    def test_convertCelsiusToKelvin(self):
+        self.assertAlmostEqual(convertCelsiusToKelvin(0), 273.15)
+        self.assertAlmostEqual(convertCelsiusToKelvin(100), 373.15)
+        self.assertAlmostEqual(convertCelsiusToKelvin(-273.15), 0)
+        self.assertAlmostEqual(convertCelsiusToKelvin(300), 573.15)
+        self.assertAlmostEqual(convertCelsiusToKelvin(37), 310.15)
+
+    def test_convertCelsiusToFahrenheit(self):
+        self.assertAlmostEqual(convertCelsiusToFahrenheit(0), 32)
+        self.assertAlmostEqual(convertCelsiusToFahrenheit(100), 212)
+        self.assertAlmostEqual(convertCelsiusToFahrenheit(-40), -40)
+        self.assertAlmostEqual(convertCelsiusToFahrenheit(37), 98.6)
+        self.assertAlmostEqual(convertCelsiusToFahrenheit(300), 572)
+
+    # Add similar test cases for other conversions...
+
+
+if __name__ == '__main__':
+    unittest.main()
